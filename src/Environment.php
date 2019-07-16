e4de33709803fd03436e65ae09edf83aa29bf350
Tianle Xu <xtl@xtlsoft.top>
2019-07-16 14:09:52 +0800

    feat(environment): add getVersion() method
    Add `Saphon\Environment::getVersion()` method to get Saphon's version.

--- src/Environment.php
+++ src/Environment.php
@@ -13,6 +13,54 @@
 
 namespace Saphon;
 
+/**
+ * Class Environment provides utilities
+ * to deal with current environment.
+ */
 class Environment {
-    
+
+    /**
+     * Cache of Saphon's version
+     *
+     * @var string
+     */
+    protected static $_version = null;
+
+    /**
+     * Get Saphon's version
+     *
+     * @return string
+     */
+    public static function getVersion(): string {
+        if (self::$_version) return self::$_version;
+        $version_file = dirname(__DIR__) . "/VERSION";
+        $version_string = '';
+        if (file_exists($version_file)) {
+            $version_string = file_get_contents($version_file);
+        } else {
+            $version_string = "0.0.0-unknown";
+        }
+        $git_head_file = dirname(__DIR__) . "/.git/HEAD";
+        if (file_exists($git_head_file)) {
+            $lines = explode("\n", trim(file_get_contents($git_head_file)));
+            $next_file = dirname(__DIR__) . "/.git/";
+            foreach ($lines as $line) {
+                if (substr($line, 0, 5) == "ref: ") {
+                    $next_file .= substr($line, 5);
+                    break;
+                }
+            }
+            $commit_hash = '';
+            if ($next_file === dirname(__DIR__) . "/.git/")
+                $commit_hash = substr(trim($lines[0]), 0, 7);
+            else
+                $commit_hash = substr(trim(file_get_contents($next_file)), 0, 7);
+            $version_string = "$version_string-git$commit_hash";
+        } else {
+            $version_string .= "-packed";
+        }
+        self::$_version = $version_string;
+        return $version_string;
+    }
+
 }
