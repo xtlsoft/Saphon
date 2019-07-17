03b40499127606f5b5a359a610d370303f5b9a56
Tianle Xu <xtl@xtlsoft.top>
2019-07-17 10:41:08 +0800

    chore(ci): add unit tests
    Integrate PHPUnit tests into this project.

--- .ci/build.php
+++ .ci/build.php
@@ -19,5 +19,15 @@ if (system($psalm_exec) === false) {
     echo "[!] Psalm check failed.\r\n";
     exit(-1);
 }
+echo "[o] Psalm check passed.\r\n";
+
+// PHPUnit check (unit tests)
+$phpunit_exec = dirname(__DIR__) . '/vendor/bin/phpunit';
+
+if (system($phpunit_exec) === false) {
+    echo "[!] PHPUnit check failed.\r\n";
+    exit(-1);
+}
+echo "[o] PHPUnit check passed.\r\n";
 
 exit(0);
