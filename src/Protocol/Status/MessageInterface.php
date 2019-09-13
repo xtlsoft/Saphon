7db0331fd89b0876833f920e0ca6ae134efc1946
Tianle Xu <xtl@xtlsoft.top>
2019-09-13 15:47:45 +0800

    feat(protocol): construct protocol class structure
    Construct class structures of Protocol namespace: including Connector, Middleware, Status and Manager.

---
+++ src/Protocol/Status/MessageInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * The file is a part of Saphon
+ * project. Please use it under
+ * its license.
+ *
+ * @package  Saphon
+ * @license  MIT
+ * @author   Tianle Xu <xtl@xtlsoft.top>
+ * @category framework
+ * @link     https://github.com/xtlsoft/Saphon/
+ */
+
+namespace Saphon\Protocol\Status;
+
+interface MessageInterface
+{ }
